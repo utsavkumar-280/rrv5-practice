3f6695965e763afbeaec290435fdf314134b973f
Utsav Kumar <utsavkumar280@gmail.com>
2021-04-07 19:14:54 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+	BrowserRouter as Router,
+	Route,
+	Link,
+	useParams,
+	Switch,
+} from "react-router-dom";
+import "./App.css";
+
+const Home = () => {
+	const { id } = useParams();
+	return (
+		<div className="comp">
+			<h1>id {id}</h1>
+		</div>
+	);
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<div className="app">
+				<ul>
+					<li>
+						<Link to="/">Home</Link>
+						<Link to="/about">About</Link>
+						<Link to="/profile">Profile</Link>
+					</li>
+				</ul>
+				<Switch>
+					<Route path="/:id">
+						<Home />
+					</Route>
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
